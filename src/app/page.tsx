910a30d2ff384b9de2ed21cd741cd0d0c6869a9f
oggy107 <urmalsingh10@gmail.com>
2024-05-24 02:04:54 +0530

    feat: add light/dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Hello there
+        <main>
+            <UserButton afterSignOutUrl="/" />
         </main>
     );
 }
