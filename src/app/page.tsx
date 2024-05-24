177be44e2066e0c80c48a0e61c6205100eb31acf
oggy107 <urmalsingh10@gmail.com>
2024-05-24 14:11:09 +0530

    feat: add theme toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import UserButton from "./components/UserButton";
+import UserButton from "@/components/auth/UserButton";
+import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
     return (
         <main>
             <UserButton afterSignOutUrl="/" />
+            <ThemeToggle />
         </main>
     );
 }
