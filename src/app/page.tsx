697f323af4e807bbc9166a3bd026efc70c7edb79
oggy107 <urmalsingh10@gmail.com>
2024-05-24 18:11:42 +0530

    feat: add setup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import UserButton from "@/components/auth/UserButton";
 import ThemeToggle from "@/components/ThemeToggle";
+import { db } from "@/lib/db";
 
-export default function Home() {
-    return (
-        <main>
-            <UserButton afterSignOutUrl="/" />
-            <ThemeToggle />
-        </main>
-    );
-}
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    if (server) {
+        redirect(`/servers/${server.id}`);
+    }
+
+    return <div>Create A Server</div>;
+};
+
+export default SetupPage;
