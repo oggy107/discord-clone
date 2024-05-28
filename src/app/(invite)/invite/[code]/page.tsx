378998eeb342a31626c78d2e619262bdc6283154
oggy107 <urmalsingh10@gmail.com>
2024-05-29 01:01:15 +0530

    feat: add invite functionality

---
+++ src/app/(invite)/invite/[code]/page.tsx
@@ -0,0 +1,55 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+const InviteCodePage = async ({ params }: { params: { code: string } }) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return auth().redirectToSignIn();
+    }
+
+    if (!params.code) {
+        return redirect("/");
+    }
+
+    const existingServer = await db.server.findFirst({
+        where: {
+            inviteCode: params.code,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    if (existingServer) {
+        return redirect(`/servers/${existingServer.id}`);
+    }
+
+    const server = await db.server.update({
+        where: {
+            inviteCode: params.code,
+        },
+        data: {
+            members: {
+                create: [
+                    {
+                        profileId: profile.id,
+                    },
+                ],
+            },
+        },
+    });
+
+    if (server) {
+        redirect(`/servers/${server.id}`);
+    }
+
+    return null;
+};
+
+export default InviteCodePage;
--- src/components/server/ServerSidebar.tsx
+++ src/components/server/ServerSidebar.tsx
@@ -64,6 +64,8 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
         (member) => member.profileId === profile.id
     )?.role;
 
+    console.log(server);
+
     return (
         <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
             <ServerHeader role={role} server={server} />
