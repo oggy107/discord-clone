9f13ff7be58cf6f30bdc38178128100f008b99dd
oggy107 <urmalsingh10@gmail.com>
2024-05-31 15:32:48 +0530

    feat: add ChatHeader

--- src/app/(main)/servers/[id]/channels/[channelId]/page.tsx
+++ src/app/(main)/servers/[id]/channels/[channelId]/page.tsx
@@ -1,9 +1,47 @@
-const ChannelIdPage = ({
-    params,
-}: {
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import ChatHeader from "@/components/chat/ChatHeader";
+
+interface ChannelIdPageProps {
     params: { id: string; channelId: string };
-}) => {
-    return <div>this is channel {params.channelId}</div>;
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return auth().redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.id,
+            profileId: profile.id,
+        },
+    });
+
+    if (!channel || !member) {
+        return redirect("/");
+    }
+
+    return (
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+        </div>
+    );
 };
 
 export default ChannelIdPage;
